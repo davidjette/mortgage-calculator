2acb8bec087ae4f9ccb497b4ef6c207d81a0a268
sydiot <david@innoventcapital.com>
2023-03-21 20:17:52 -0700

    Update code with correct calc and amort table 1.1

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-const loanAmountSlider = document.getElementById('loanAmount');
-const interestRateSlider = document.getElementById('interestRate');
-const loanTermSlider = document.getElementById('loanTerm');
-const loanAmountOutput = document.getElementById('loanAmountOutput');
-const interestRateOutput = document.getElementById('interestRateOutput');
-const loanTermOutput = document.getElementById('loanTermOutput');
-const calculateBtn = document.getElementById('calculateBtn');
-const payoffAmount = document.getElementById('payoffAmount');
-
-function updateLoanAmount() {
-  loanAmountOutput.innerText = `$${loanAmountSlider.value}`;
-}
-
-function updateInterestRate() {
-  interestRateOutput.innerText = `${interestRateSlider.value}%`;
-}
-
-function updateLoanTerm() {
-  loanTermOutput.innerText = `${loanTermSlider.value} years`;
-}
-
 function calculateMortgage() {
     const principal = parseFloat(loanAmountSlider.value);
     const annualInterestRate = parseFloat(interestRateSlider.value) / 100;
     const loanTermInYears = parseInt(loanTermSlider.value);
   
-    const interestPaid = principal * annualInterestRate * loanTermInYears;
-    const payoff = principal + interestPaid;
+    // Calculate monthly payment
+    const monthlyInterestRate = annualInterestRate / 12;
+    const numberOfPayments = loanTermInYears * 12;
+    const monthlyPayment = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
   
+    // Calculate compound interest and generate amortization table
+    let balance = principal;
+    const amortizationTable = document.getElementById('amortizationTable').getElementsByTagName('tbody')[0];
+    amortizationTable.innerHTML = '';
+  
+    for (let i = 1; i <= numberOfPayments; i++) {
+      const interestPayment = balance * monthlyInterestRate;
+      const principalPayment = monthlyPayment - interestPayment;
+      balance -= principalPayment;
+  
+      const newRow = amortizationTable.insertRow();
+      newRow.insertCell().innerText = i;
+      newRow.insertCell().innerText = `$${principalPayment.toFixed(2)}`;
+      newRow.insertCell().innerText = `$${interestPayment.toFixed(2)}`;
+      newRow.insertCell().innerText = `$${balance.toFixed(2)}`;
+    }
+  
+    const payoff = principal + (monthlyPayment * numberOfPayments);
     payoffAmount.innerText = `$${payoff.toFixed(2)}`;
   }
   
-
-loanAmountSlider.addEventListener('input', updateLoanAmount);
-interestRateSlider.addEventListener('input', updateInterestRate);
-loanTermSlider.addEventListener('input', updateLoanTerm);
-calculateBtn.addEventListener('click', calculateMortgage);
-
-updateLoanAmount();
-updateInterestRate();
-updateLoanTerm();
+  loanAmountSlider.addEventListener('input', updateLoanAmount);
+  interestRateSlider.addEventListener('input', updateInterestRate);
+  loanTermSlider.addEventListener('input', updateLoanTerm);
+  calculateBtn.addEventListener('click', calculateMortgage);
+  
+  updateLoanAmount();
+  updateInterestRate();
+  updateLoanTerm();
+  
